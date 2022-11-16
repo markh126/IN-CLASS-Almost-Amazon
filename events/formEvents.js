e1852be75738e30928023f872a71c4c585e82116
Mark Hamilton <ham.mark126@gmail.com>
2022-11-15 21:33:25 -0600

    added favorite check

--- events/formEvents.js
+++ events/formEvents.js
@@ -53,6 +53,7 @@ const formEvents = () => {
         email: document.querySelector('#email').value,
         first_name: document.querySelector('#first_name').value,
         last_name: document.querySelector('#last_name').value,
+        favorite: document.querySelector('#favorite').checked,
       };
 
       createAuthor(payload).then(({ name }) => {
@@ -70,6 +71,7 @@ const formEvents = () => {
         email: document.querySelector('#email').value,
         first_name: document.querySelector('#first_name').value,
         last_name: document.querySelector('#last_name').value,
+        favorite: document.querySelector('#favorite').checked,
         firebaseKey,
       };
 
